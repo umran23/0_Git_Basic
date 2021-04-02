361c36a11ef61e6f221af73c6a30807f5414cc0e
umran23 <umran.bac@gmail.com>
2021-04-02 03:40:28 +0300

    Editing files

---
+++ main.py
@@ -0,0 +1,16 @@
+#this code wirtten in Pyhthon
+
+a=5
+b=4
+#this is the first commit add by main branch
+print("Sum = ",a+b)
+#this is the second commit add by main branch
+print("Mult = ",a*b)
+#this is the first commit added by branch alhaja
+print("DIV = ", a/b)
+
+#this is added by Gust Branch but conflits solved and we need to merge it 
+#with main branch
+print(" Hello Gust!, Don't make conflicts again please!")
+print("Sorry!!!!!, i didn't want to make conflicts ")
+print("It take me about one hour to solve this shit -_-")
